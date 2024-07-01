30a56eaa89361caac89f6a40301744326b86be67
amin-ni88 <amin_me@live.com>
2024-07-01 11:51:52 +0330

    update modele

--- back_end_johari/polls/models.py
+++ back_end_johari/polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class JSONField:
     pass
 
-
+#models
 class Person(models.Model):
     id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=200)
