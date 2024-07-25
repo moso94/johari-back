11dfd8f08173bb6de5315873c4c14dcaa1c5c06f
Amin <54573986+amin-ni88@users.noreply.github.com>
2024-07-25 18:54:38 +0330

    models add

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(blank = True, null=True, max_length=100)
+    email = models.EmailField(null = True, unique = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class Project(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.CharField(unique=True, max_length=1024)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    
