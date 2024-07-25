9d20bc85397b1a4b06524f956323f1fe4606db14
Amin <54573986+amin-ni88@users.noreply.github.com>
2024-07-25 19:14:26 +0330

    complet models

--- api/models.py
+++ api/models.py
@@ -5,11 +5,27 @@ class User(models.Model):
     email = models.EmailField(null = True, unique = True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Adjective(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(unique=True, max_length=100)
+
 class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.CharField(unique=True, max_length=1024)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    adectives = models.ManyToManyField(Adjective)
+
+class FeedBack(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    adectives = models.ManyToManyField(Adjective)
+
 
 
     
