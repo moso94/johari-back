0919556858f41b12523a382cc79e6f5e1e2758a5
Amin <54573986+amin-ni88@users.noreply.github.com>
2024-07-28 17:45:46 +0330

    api rest

---
+++ api/serializers.py
@@ -0,0 +1,12 @@
+from .models import User, Adjective
+from rest_framework import serializers
+
+# class UserSerializers(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', '' 'created_at']
+class AdjectiveSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Adjective
+        fields = ['id', 'title', 'created_at', 'updated_at']
+    
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Adjective
+from .serializers import AdjectiveSerializers
+from rest_framework import permissions, viewsets
+
+
+class AdjectiveViewSet(viewsets.ModelViewSet):
+    queryset = Adjective.objects.all().order_by('-id')
+    serializer_class = AdjectiveSerializers
+    permission_classes = [permissions.AllowAny]
+
 
 
