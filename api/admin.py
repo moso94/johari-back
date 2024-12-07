059516bd9d8aa6b329d4cc1178ced5a5ef9513db
Amin Nikbakht <amin_me@live.com>
2024-12-07 11:18:01 +0330

    hello guy

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "email", "created_at", "updated_at")
+
 
 admin.site.register(User, UserAdmin)
 
