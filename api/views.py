e0b0be61c8592fe7bfc60a801d7f842fa0954ff7
amin_ni88 <amin_me@live.com>
2024-12-16 20:46:07 +0330

    g

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
+import django_filters.rest_framework
 from rest_framework import permissions, viewsets
-
 from .models import Adjective, User
 from .serializers import AdjectiveSerializers, UserSerializers
 
@@ -8,6 +8,7 @@ class AdjectiveViewSet(viewsets.ModelViewSet):
     queryset = Adjective.objects.all().order_by('-id')
     serializer_class = AdjectiveSerializers
     permission_classes = [permissions.AllowAny]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
 
 
@@ -15,6 +16,7 @@ class AdjectiveListViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Adjective.objects.all()
     serializer_class = AdjectiveSerializers
     permission_classes = [permissions.AllowAny]
+
     def get_queryset(self):
         return Adjective.objects.all()
 
@@ -24,11 +26,14 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-id')
     serializer_class = UserSerializers
     permission_classes = [permissions.AllowAny]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
 
 class UserListViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by('-id')
     serializer_class = UserSerializers
     permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['name', 'email']
     def get_queryset(self):
         return User.objects.all()
