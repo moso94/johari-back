3801441519241edd576b169b8346def3f3fc7cf1
Amin Nikbakht <amin_me@live.com>
2025-01-04 11:49:38 +0330

    hello guy

--- api/views.py
+++ api/views.py
@@ -1,7 +1,21 @@
+from cProfile import Profile
+
 import django_filters.rest_framework
 from rest_framework import permissions, viewsets
-from .models import Adjective, User
-from .serializers import AdjectiveSerializers, UserSerializers
+from .models import Adjective, User, Project
+from .serializers import AdjectiveSerializers, UserSerializers, ProjectSerializers
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+
+
+
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class AdjectiveViewSet(viewsets.ModelViewSet):
@@ -9,7 +23,9 @@ class AdjectiveViewSet(viewsets.ModelViewSet):
     serializer_class = AdjectiveSerializers
     permission_classes = [permissions.AllowAny]
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    filterset_fields = ['title']  
+    filterset_fields = ['title']
+    search_fields = ['title']  
+    pagination_class = StandardResultsSetPagination
 
 
 class AdjectiveListViewSet(viewsets.ReadOnlyModelViewSet):
@@ -23,13 +39,12 @@ class AdjectiveListViewSet(viewsets.ReadOnlyModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-id')
     serializer_class = UserSerializers
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticated] 
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ['name', 'email']  
 
 
 class UserListViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all().order_by('-id')
     serializer_class = UserSerializers
     permission_classes = [permissions.IsAdminUser]
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
@@ -37,3 +52,60 @@ class UserListViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return User.objects.all()
+
+class UserDetailViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['name', 'email']
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = UserSerializers
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['name', 'email']
+
+
+    def get_queryset(self):
+        return Profile.object.all()
+
+class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['title']
+
+    def get_queryset(self):
+        return Project.object.all()
+
+
+class ProjectListViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['title']
+
+    def get_queryset(self):
+        return Project.object.all()
+
+
+class ProjectDetailViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+    permission_classes = [permissions.IsAdminUser]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['title']
+
+    def get_queryset(self):
+        return Project.object.all()
+
+
+
